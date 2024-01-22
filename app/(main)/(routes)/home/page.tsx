eb3cefe90c1946853d281c10845b72b1a816be92
pisoc912 <shenyf1124@outlook.com>
2024-01-21 23:08:30 -0800

    config prisma db

--- app/(main)/(routes)/home/page.tsx
+++ app/(main)/(routes)/home/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 const Home = () => {
     return ( 
         <p>This is an protected page
             <UserButton afterSignOutUrl="/" />
+        <ModeToggle />
         </p>
      );
 }
