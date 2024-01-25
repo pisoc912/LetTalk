f87257dd776b696f0b7b4a865eba5c8d07315786
pisoc912 <shenyf1124@outlook.com>
2024-01-24 21:11:45 -0800

    add server search feature

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,32 +1,34 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-import { CreateServerModal } from "../modals/create-server-modal"
-import { InviteModal } from "../modals/invite-modal"
-import { EditServerModal } from "../modals/edit-server-model"
-import { MembersModal } from "../modals/members-modal"
-import { CreateChannelModal } from "../modals/create-channel-modal"
-import { LeaveModal } from "../modals/leave-server-modal"
+import { CreateServerModal } from "../modals/create-server-modal";
+import { InviteModal } from "../modals/invite-modal";
+import { EditServerModal } from "../modals/edit-server-model";
+import { MembersModal } from "../modals/members-modal";
+import { CreateChannelModal } from "../modals/create-channel-modal";
+import { LeaveModal } from "../modals/leave-server-modal";
+import { DeleteServer } from "../modals/delete-server-modal";
 
-export const ModalProvider = ()=>{
-    const [isMounted, setIsMounted] = useState(false)
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(()=>{
-        setIsMounted(true)
-    },[])
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    if(!isMounted){
-        return null;
-    }
-    return (
-        <>
-        <CreateServerModal />
-        <InviteModal />
-        <EditServerModal />
-        <MembersModal />
-        <CreateChannelModal />
-        <LeaveModal />
-        </>
-    )
-}
+  if (!isMounted) {
+    return null;
+  }
+  return (
+    <>
+      <CreateServerModal />
+      <InviteModal />
+      <EditServerModal />
+      <MembersModal />
+      <CreateChannelModal />
+      <LeaveModal />
+      <DeleteServer />
+    </>
+  );
+};
