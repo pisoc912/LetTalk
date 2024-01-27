635c92aec862cef6b9ec261a560530405aea84cf
pisoc912 <shenyf1124@outlook.com>
2024-01-27 00:01:04 -0800

    finish socket config

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from '@/components/chat/chat-header';
+import ChatInput from '@/components/chat/chat-input';
 import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { redirectToSignIn } from '@clerk/nextjs';
@@ -31,10 +32,24 @@ const ChannelIdPage = async({params:{serverId,channelId}}:ChannelIdProps) => {
     redirect("/")
   }
     return (
-    <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
-        <ChatHeader serverId={channel.serverId} type="channel" name={channel.name} />
-    </div>
-  )
+      <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+        <ChatHeader
+          serverId={channel.serverId}
+          type="channel"
+          name={channel.name}
+        />
+        <div className="flex-1">Future Message</div>
+        <ChatInput
+          name={channel.name}
+          type="channel"
+          apiUrl="/api/socket/messages"
+          query={{
+            channelId: channel.id,
+            serverId:channel.serverId
+          }}
+        />
+      </div>
+    );
 }
 
 export default ChannelIdPage
---
+++ components/socket-indicator.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import React from 'react'
+import { Badge } from './ui/badge'
+import { useSocket } from './providers/socket-provider'
+
+export const SocketIndicator = () => {
+    const {isConnected} = useSocket()
+    if(!isConnected){
+        return(
+            <Badge variant="outline" className='bg-yellow-600 text-white border-none'>
+                Fallback: Polling every 1s
+            </Badge>
+        )
+    }
+  return (
+    <Badge variant="outline" className='bg-emerald-600 text-white border-none'>
+        Live:Real-time updates
+    </Badge>
+  )
+}
