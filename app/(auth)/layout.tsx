e76058a8e7e47c0eeda7dc9b1667e6dd15a34480
pisoc912 <shenyf1124@outlook.com>
2024-01-29 19:41:35 -0800

    update background

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,18 +5,6 @@ const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="authLayout">
       <div className="backgroundImage" />
-      {/* <div className="authLayoutContent">
-          <h1>Imaging a placing...</h1>
-        <p>
-          ...where you can belong to any school club, any gaming group, or a
-          worldwide art community.
-        </p>
-        <p>Where just you and anyone can spend time together.</p>
-        <p>
-          A place where strangers can become acquaintances...or maybe even
-          friends.
-        </p>
-      </div> */}
       {children}
     </div>
   );
